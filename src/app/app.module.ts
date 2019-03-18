9527eff7dd61bb8c85e2b48f3ef0b5f90c7a7445
BlackCarDriver <13169346002@163.com>
2019-03-18 22:22:58 +0800

    working on personal page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { PersonalComponent } from './personal/personal.component';
 import { NavigComponent } from './navig/navig.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
     PersonalComponent,
-    NavigComponent
+    NavigComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
